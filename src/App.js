49e7e00a05899477095b5e40e02c7bf11cec0cd2
Mina Huh <minarainbow@kaist.ac.kr>
2018-12-19 17:10:12 +0900

    working on API summary

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-// import person from '@material-ui/icons/person';
+import PersonIcon from '@material-ui/icons/Person';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -16,9 +16,15 @@ import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import Markdown from './Markdown';
 import logo from './logo.png';
+import MicBlend from './MicBlend.png';
+import Emotions from './Emotions.png';
+import Customizing from './Customizing.png';
+import MultiLingual from './MultiLingual.png';
+
 
 
 const styles = theme => ({
+  
   layout: {
     width: 'auto',
     marginLeft: theme.spacing.unit * 3,
@@ -37,13 +43,37 @@ const styles = theme => ({
     flex: 1,
     color: theme.palette.grey[600],
   },
-  toolbarSecondary: {
+  toolbarMenu: {
+    width: 400,
+    margin: 'auto',
+    textAlign: 'center',
     justifyContent: 'space-between',
+    marginTop: theme.spacing.unit,
   },
-  mainFeaturedPost: {
-    backgroundColor: theme.palette.grey[800],
-    color: theme.palette.common.white,
-    marginBottom: theme.spacing.unit * 4,
+  mypageIcon: {
+    height:50,
+    width:50,
+  },
+  container: {
+    position: 'relative',
+    marginBottom: 50
+  },
+  container2: {
+    textAlign: 'center',
+  },
+  summaryAPI: {
+    // fontFamily: '굴림',
+    position: 'absolute',
+    top: 70,
+    left: 200,
+    color: 'white',
+  },
+  detailAPI: {
+    // fontFamily: '굴림',
+    position: 'absolute',
+    top: 140,
+    left: 200,
+    color: 'white',
   },
   mainFeaturedPostContent: {
     padding: `${theme.spacing.unit * 6}px`,
@@ -117,12 +147,38 @@ class App extends Component {
               align="left"
               noWrap
               className={classes.toolbarTitle}>
-              Humelo
+              Prosody TTS
             </Typography>
-            <IconButton class="material-icons" color="action">
-              person
+            <IconButton className={classes.mypageIcon} color="action">
+              <PersonIcon  style={{fontSize: '30px'}}/>
             </IconButton>
           </Toolbar>
+          <Toolbar variant="dense" className={classes.toolbarMenu}>
+          {sections.map(section => (
+            <Button color="inherit" noWrap key={section}>
+              {section}
+            </Button>
+          ))}
+        </Toolbar>
+        <main>
+            <div className={classes.container}  width="auto">
+              <img src={MicBlend} alt="MicBlend" width="100%"/>
+              <Typography className={classes.summaryAPI} variant="h4">
+              맞춤형 목소리 합성 API
+              </Typography>
+              <Typography className={classes.detailAPI} variant="h6">
+              사람의 감정을 담아서 말할 수 있는 자연스러운 TTS API는<br />
+              IoT, 게임, 어플리케이션 등의 다양한 분야에서 사용될 수 있습니다<br /><br />
+              풍부한 감정을 가지고 말하는 인공지능을 만나보세요
+              </Typography>
+            </div>
+            <div className={classes.container2}  width="auto">
+              Humelo TTS는 무엇이 다른가요?<br />
+              <img src={Emotions} alt="Emotions" width="30%"/>
+              <img src={Customizing} alt="Customizing" width="30%"/>
+              <img src={MultiLingual} alt="Multilingual" width="30%"/>
+            </div>
+        </main>
         </div>
       </React.Fragment>
     );
